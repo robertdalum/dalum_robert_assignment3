13a3f9ac7cef2571cd05d8ad6307f4ff69558c67
robertdalum <robertdalum@yahoo.com>
2013-08-22 12:54:33 -0500

    Half way done added var and conditionals

---
+++ rename-me.js
@@ -0,0 +1,27 @@
+alert("JavaScript works!");
+
+//Robert Dalum
+//Assignment 3
+// August 22, 2013
+
+var workGoals = {
+	proCards = "11%",
+	reservations : "22%"
+};
+
+console.log(workGoals);
+
+var tradeItems = true
+
+if (tradeItems === true){
+	console.log("Customers trading in items helps us towards reserves and cards!")
+}else{
+	console.log("If people don't come into the store with trades it is a little harders to get reserves or cards. ")
+};
+
+if (tradeItems === true){
+	console.log("Once customers get a pro card they tend to go ahead and buy more games which leads to higher sales!")
+}else{
+	console.log("Lower sales, I am really really sad.")
+};
+
